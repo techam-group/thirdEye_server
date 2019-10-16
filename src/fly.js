012523c605db9e8aa09f2a6e021687d6ead0a289
Favour George C <phavorsparks@gmail.com>
2019-10-16 23:08:01 +0100

    updates

--- src/fly.js
+++ src/fly.js
@@ -13,16 +13,22 @@ const HOST = '192.168.10.1';
 const drone = dgram.createSocket('udp4');
 drone.bind(PORT);
 
-const _3rdEye = dgram.createSocket('udp4');
-_3rdEye.bind(DS_PORT);
+const droneState = dgram.createSocket('udp4');
+droneState.bind(DS_PORT);
 
 drone.on('message', message => {
   console.log(`🤖 : ${message}`);
 });
 
-_3rdEye.on('message', message => {
-  console.log(`🤖 : ${message}`)
-})
+// droneState.on('message', state => {
+//   console.log(state.toString())
+//   const formattedState = parseState(state)
+//   console.log(`🤖 : ${formattedState}`)
+// })
+
+function parseState(state) {
+  return state.split(';').map(x => x.split(':'))
+}
 
 function handleError(err) {
   if (err) {
@@ -34,7 +40,7 @@ function handleError(err) {
 // drone.send('command', 0, 7, PORT, HOST, handleError)
 // drone.send('battery?', 0, 8, PORT, HOST, handleError)
 
-const commands = ['command', 'battery?']
+const commands = ['command', 'battery?', 'time?']
 
 let i = 0;
 
@@ -47,7 +53,7 @@ async function go() {
   drone.send(command, 0, command.length, PORT, HOST, handleError)
 
   await wait(delay)
-  i++
+  i += 1
 
   if (i < commands.length) {
     return go()
